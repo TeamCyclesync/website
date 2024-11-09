9210383fc2006fd9576f6bee5ee19023c52e038d
Lakshdeep Singh <lakshdeep.code@gmail.com>
2024-11-09 12:24:45 +0530

    Improved Responsiveness of Bento Grid

--- src/components/landing/Bento.tsx
+++ src/components/landing/Bento.tsx
@@ -2,18 +2,37 @@
 
 import Image from 'next/image'
 import { FaHandFist } from "react-icons/fa6";
-import { PiClockCountdownFill } from "react-icons/pi";
 import { GiDna2 } from "react-icons/gi";
 import { IoHourglassOutline } from "react-icons/io5";
 import { Button } from '../ui/button';
 import { FiDownload } from "react-icons/fi";
 import { FaCalendarCheck } from "react-icons/fa6";
+import {
+    motion,
+    useInView
+} from 'framer-motion'
+import { useRef } from 'react';
+
 
 const Bento = () => {
-
+    const containerRef = useRef(null);
+    const isInView = useInView(containerRef, {
+        amount: 0.5,
+        once: true
+    });
     return (
-        <div className='p-[1rem] -mt-4 sm:mt-10'>
-                
+        <motion.div className='p-[1rem] -mt-4 sm:mt-10 xsm:max-sm:px-[3rem]'
+            ref={containerRef}
+            initial={{
+                opacity: 0,
+                translateY: "20px"
+            }}
+            animate={{
+                opacity: isInView ? 1 : 0,
+                translateY: isInView ? 0 : "20px"
+            }}
+        >
+
             <div className="flex flex-col gap-2 max-md:w-full max-w-[850px] mx-auto text-center mb-10">
                 <h1 className="md:text-[2.5rem] sm:text-[2rem] text-[1.5rem] tracking-tighter font-semibold text-white text-shadow-200">Change your Life</h1>
                 <p className="md:text-[1rem] mx-auto text-[0.875rem] leading-5 sm:max-md:w-[600px] md:max-w-[650px] text-shadow-200 text-white">
@@ -53,11 +72,11 @@ const Bento = () => {
 
                 {/* Sub section - 1 */}
                 <div className='max-sm:col-span-1 fully-centered rounded-xl p-4 hover:scale-[1.03] duration-300 transition-scale ease-in-out bg-amber-300 group relative overflow-hidden'>
-                <IoHourglassOutline className='absolute text-white/30' size={200}/>
+                    <IoHourglassOutline className='absolute text-white/30' size={200} />
 
                     <div className='flex flex-col text-center z-[13]'>
-                        <span className='tracking-tighter text-[3rem] font-extrabold text-white text-shadow-200 leading-[50px] group-hover:scale-[1.1] duration-300 transition-all'>20%</span>
-                        <span className='text-white font-bold tracking-tight uppercase text-[1rem] text-shadow-200'><span className='text-violet-'>Increase</span> in Productivity</span>
+                        <span className='tracking-tighter text-[2rem] md:text-[3rem] font-extrabold text-white text-shadow-200 leading-[50px] group-hover:scale-[1.1] duration-300 transition-all'>20%</span>
+                        <span className='text-white font-bold tracking-tight uppercase leading-[18px] text-[0.875rem] md:text-[1rem] text-shadow-200'><span className='text-violet-'>Increase</span> in Productivity</span>
                     </div>
                 </div>
 
@@ -75,33 +94,31 @@ const Bento = () => {
                     <GiDna2 className='absolute text-white/20 -right-[5%] group-hover:scale-[1.08] transition-all duration-300 ease' size={260} />
 
                     {/* Image */}
-                    <div className='absolute h-[250px] w-[250px] left-[-8%] bottom-[-35%] group-hover:left-[-5%] transition-all dutation-300 ease grayscale group-hover:grayscale-0 group-hover:scale-[1.03]'>
+                    <div className='absolute md:h-[250px] h-[200px] w-[200px] md:w-[250px] left-[-8%] bottom-[-35%] group-hover:left-[-5%] transition-all dutation-300 ease grayscale group-hover:grayscale-0 group-hover:scale-[1.03]'>
                         <Image src={"/assets/running.png"} alt='Women running' height={500} width={600} className='object-cover h-full w-full' />
                     </div>
 
-                    <h3 className='text-[2rem] text-white font-bold tracking-tighter text-right leading-7 text-shadow-200 absolute right-[5%] bottom-[35%]'>
+                    <h3 className='text-[1.5rem] md:text-[2rem] text-white font-bold tracking-tighter text-right leading-7 text-shadow-200 absolute right-[5%] bottom-[35%] leading-[15px]'>
                         Work with your Biology
                     </h3>
-                    <p className='absolute bottom-[10%] right-[5%] text-violet-700 tracking-tight font-bold text-[1rem] uppercase'>
+                    <p className='absolute bottom-[10%] right-[5%] text-violet-700 tracking-tight font-bold text-[0.875rem] md:text-[1rem] uppercase'>
                         Not against it
                     </p>
                 </div>
 
                 {/* section - 2 */}
-                <div className='max-sm:col-span-2 col-span-2 fully-centered bg-white rounded-xl bg-purple-300 relative overflow-hidden group hover:scale-[1.03] transition-all duration-300 ease'>
+                <div className='max-sm:col-span-2 col-span-2 fully-centered rounded-xl bg-purple-300 relative overflow-hidden group hover:scale-[1.03] transition-all duration-300 ease'>
 
-                    <PiClockCountdownFill size={60} className='text-white drop-shadow-md absolute left-[4%] top-[10%] group-hover:scale-[1.05] transition-all duration-300 ease' />
-
-                    <div className='absolute w-[150px] h-[200px] bottom-[-15%] left-[3%] group-hover:left-[5%] transition-all duration-300 ease grayscale group-hover:grayscale-0 group-hover:scale-[1.03]'>
+                    <div className='absolute max-sm:w-[100px] max-sm:h-[150px] w-[150px] h-[200px] bottom-[-15%] left-[-2%] xsm:left-[3%] group-hover:left-[5%] transition-all duration-300 ease grayscale group-hover:grayscale-0 group-hover:scale-[1.03]'>
                         <Image src={"/assets/track.png"} alt='Tracking Cycle' width={150} height={200} className='object-contain' />
                     </div>
 
                     <div className='flex flex-col gap-1 absolute right-[5%] top-[15%] text-right'>
-                        <h3 className='text-white text-shadow-200 font-bold leading-8 text-[2rem] tracking-tighter'>Track Your Cycles <br />with Ease</h3>
-                    </div>  
+                        <h3 className='text-white text-shadow-200 font-semibold md:leading-8 text-[1.5rem] leading-[25px] md:text-[2rem] tracking-tighter'>Track Your Cycles <br className='xsm:max-sm:hidden' />with Ease</h3>
+                    </div>
 
-                    <div className='flex gap-2 items-center absolute bottom-[10%] right-[5%]'>
-                        <Button className='hover:scale-[1.03] hover:bg-purple-700 hover:shadow-lg transition-all  bg-purple-600 duration-300 ease' size={'sm'}><FiDownload/> Download the App</Button>
+                    <div className='gap-2 items-center absolute bottom-[10%] right-[5%] flex'>
+                        <Button className='hover:scale-[1.03] hover:bg-purple-700 hover:shadow-lg transition-all  bg-purple-600 duration-300 ease' size={'sm'}><FiDownload /> Download the App</Button>
 
                         <Button size={'sm'} className='flex gap-2 items-center hover:scale-[1.03] transition-all duration-300 ease'>
                             <FaCalendarCheck />
@@ -110,7 +127,7 @@ const Bento = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
